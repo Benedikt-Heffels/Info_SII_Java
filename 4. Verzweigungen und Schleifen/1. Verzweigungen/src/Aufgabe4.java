46a79dea26d0282add7d923ca35b63dc34d8bed0
Benedikt-Heffels <benedikt.heffels@gmail.com>
2021-10-31 12:13:19 +0100

    Aufgabe 4 (Was ist... (Ascii)) eingefügt.

--- 4. Verzweigungen und Schleifen/1. Verzweigungen/src/Aufgabe4.java
+++ 4. Verzweigungen und Schleifen/1. Verzweigungen/src/Aufgabe4.java
@@ -5,8 +5,22 @@ public class Aufgabe4 {
         System.out.println("Dieses Programm sagt Ihnen, was Sie eigegeben haben");
         System.out.println("Ihre Eingabe:");
         Scanner scanner = new Scanner(System.in);
-
-        //char einskennen ascii code
-        System.out.println((int) 'a');
+        String eingabe = scanner.next();
+        int ascii_zeichen = (int) (eingabe.charAt(0));
+        if (ascii_zeichen >= 33 && ascii_zeichen <= 47
+                || ascii_zeichen >= 58 && ascii_zeichen <= 64
+                || ascii_zeichen >= 91 && ascii_zeichen <= 96
+                || ascii_zeichen >= 123 && ascii_zeichen <= 126){
+            System.out.println(eingabe + " ist ein Sonderzeichen");
+        }
+        else if (ascii_zeichen >= 48 && ascii_zeichen <= 57) {
+            System.out.println(eingabe + " ist eine Zahl!");
+        }
+        else if (ascii_zeichen >= 65 && ascii_zeichen <= 90) {
+            System.out.println(eingabe + " ist ein Großbuchstabe");
+        }
+        else if (ascii_zeichen >= 97 && ascii_zeichen <= 122) {
+            System.out.println(eingabe + " ist ein Kleinbuchstabe!");
+        }
     }
 }
