913f11be85e78581291f949485ef24da9380b8c1
Benedikt-Heffels <benedikt.heffels@gmail.com>
2021-12-06 22:29:13 +0100

    Ordnung + in Datei schreiben

---
+++ Z. Codeexperimente/Z.2 Aus txt importieren/src/test.java
@@ -0,0 +1,33 @@
+//https://www.youtube.com/watch?v=PmJcd9ZyBf4
+//import java.nio.file.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        // Path path = Path.of("D:/JavaProjects/Info_SII_Java/test/src/txtdatei.txt");
+        FileWriter writer;
+        //File datei = new File("C: \Daten\1. Schule\10. EF\11. Informatik\JavaProjects\Info_SII_Java\Z. Codeexperimente" +
+                //"\Z.2 Aus txt importieren\src\textdatei.txt");
+        File datei = new File("Z. Codeexperimente/Z.2 Aus txt importieren/textdatei.txt");
+        /*try {
+            writer = new FileWriter(datei);
+            writer.write("Hallooooo");
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+         */
+        try {
+            Scanner scan = new Scanner(datei);
+            while (scan.hasNext()) { // Exception?!
+                System.out.print(scan.next() + " ");
+                scan.close();
+            }
+        }catch (IOException e) {
+            System.out.println("ERROR");
+        }
+    }
+}
+
