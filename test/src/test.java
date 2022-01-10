b16edd2cf3d6beecfbaf6448fd70bed10c610b9b
Benedikt-Heffels <benedikt.heffels@gmail.com>
2022-01-10 22:30:07 +0100

    V 1.1.0.0 HueckTech (Sicherheitsupdate/Unsichtabre Eingabe) Console

--- test/src/test.java
+++ test/src/test.java
@@ -1,11 +1,16 @@
+//import java.io.Console;
+
 public class test {
     public static void main(String[] args) {
         //Console console = System.console();
 
         System.out.println("Passwort eingeben: ");
         //char[] password = console.readPassword();
-        char[] password = System.console().readPassword();
-        System.out.println(password);
+        //char[] password = System.console().readPassword();
+        char array[] = {'H', 'A', 'L', 'L', 'O'};
+        String password_STR = String.valueOf((char[]) array);
+        System.out.println(password_STR);
+        //System.out.println(password);
 
     }
 }
