75fc48012688979bf990648fdbc68d597145faee
Benedikt-Heffels <benedikt.heffels@gmail.com>
2022-09-18 15:47:50 +0200

    JavaFX: Ratespiel

---
+++ 03. Q1/01. JavaFX/src/_05_Programme_Unterricht/_01_Ratespiel/RS_Controller.java
@@ -0,0 +1,49 @@
+package _05_Programme_Unterricht._01_Ratespiel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
+import java.util.Random;
+
+public class RS_Controller {
+    @FXML
+    Button bt_Start;
+    @FXML
+    Button bt_Eingabe;
+    @FXML
+    TextField tf_Eingabe;
+    @FXML
+    Label lbl_Lösung;
+
+    private int gesuchteZahl;
+    private int anzahlVersuche;
+
+    public void bt_Start_onClick(ActionEvent actionEvent){
+        Random random = new Random();
+        gesuchteZahl = random.nextInt(0, 100);
+        lbl_Lösung.setText("Zu suchende Zahl wurde ermittelt. Viel Glück beim finden!");
+    }
+
+
+    public void bt_Eingabe_onClick(ActionEvent actionEvent){
+        anzahlVersuche += 1;
+        //int eingabe = Integer.valueOf(tf_Eingabe.getText().toString());
+        if (Integer.valueOf(tf_Eingabe.getText().toString()) == gesuchteZahl){
+            lbl_Lösung.setText
+                    ("Sie haben in " + anzahlVersuche + " Versuchen die gesuchte Zahl "+ gesuchteZahl + " erraten! Glückwunsch!");
+        }
+        else if (Integer.valueOf(tf_Eingabe.getText().toString()) < gesuchteZahl) {
+            lbl_Lösung.setText("Die gesuchte Zahl ist größer als Ihre Eingabe!");
+        }
+        else if (Integer.valueOf(tf_Eingabe.getText().toString()) > gesuchteZahl) {
+            lbl_Lösung.setText("Die gesuchte Zahl ist kleiner als Ihre Eingabe!");
+        }
+        else{ //Die Kontroll-Abfrage verursacht noch Fehler!
+            lbl_Lösung.setText("Eingabe konnte nicht gelesen werden! Bitte geben Sie eine Zahl zwischen 0 und 100 ein!");
+            anzahlVersuche -= 1;
+        }
+    }
+}
---
+++ 03. Q1/01. JavaFX/src/_05_Programme_Unterricht/_01_Ratespiel/RS_Start.java
@@ -0,0 +1,22 @@
+package _05_Programme_Unterricht._01_Ratespiel;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RS_Start extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("RS_FXML.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Ratespiel");
+        primaryStage.show();
+    }
+}
