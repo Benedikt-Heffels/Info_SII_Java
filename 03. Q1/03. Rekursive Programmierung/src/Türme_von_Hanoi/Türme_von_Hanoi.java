ae92cc947d14d7d1b850ade6758a46d55c4d0e67
Benedikt-Heffels <70285234+Benedikt-Heffels@users.noreply.github.com>
2022-12-05 10:48:31 +0100

    Update Türme_von_Hanoi.java
    Generierung der Türme

--- 03. Q1/03. Rekursive Programmierung/src/Türme_von_Hanoi/Türme_von_Hanoi.java
+++ 03. Q1/03. Rekursive Programmierung/src/Türme_von_Hanoi/Türme_von_Hanoi.java
@@ -1,4 +1,24 @@
 package Türme_von_Hanoi;
 
 public class Türme_von_Hanoi {
+
+	public Türme_von_Hanoi(int anzahlScheiben) {
+		TurmA = new TVH_Stack();
+		TurmB = new TVH_Stack();
+		TurmC = new TVH_Stack();
+		scheibeHinzufügen(anzahlScheiben);
+	}
+
+	private TVH_Stack <Integer> TurmA;
+	private TVH_Stack <Integer> TurmB;
+	private TVH_Stack <Integer> TurmC;
+
+	protected void scheibenHinzufügen(int anzahlScheiben) {
+		for (int i = anzahlScheiben, i <=0, i--) {
+			TurmA.push(i)
+		}
+	}
+
+	public void move(S
 }
+
