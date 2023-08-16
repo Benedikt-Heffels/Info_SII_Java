b10ed706a722d00a231733741b1034be95413bc0
Benedikt-Heffels <benedikt.heffels@gmail.com>
2023-08-16 23:31:08 +0200

    Synchronisierung mit Stick (Q1)

---
+++ 03. Q1/04. Baueme/src/Binary_Tree/Binary_Tree_mit_Luecken/Entry.java
@@ -0,0 +1,24 @@
+package Binary_Tree.Binary_Tree_mit_Luecken;
+public class Entry implements ComparableContent<Entry> {
+  private int wert;
+  
+  public Entry (int Zahl) {
+    wert = Zahl;
+  }
+
+  public int getWert() {
+    return wert;
+  }
+  
+  public boolean isLess(Entry pContent) {
+    return (getWert() < pContent.getWert());
+  }
+  
+  public boolean isEqual(Entry pContent) {
+    return (getWert() == pContent.getWert());
+  }
+  
+  public boolean isGreater(Entry pContent) {
+   return (getWert() > pContent.getWert());
+  }
+}
